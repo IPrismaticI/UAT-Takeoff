010e7a51387082c426c1f4160e2fbf44e7882587
IPrismaticI <112722043+IPrismaticI@users.noreply.github.com>
2022-09-28 13:31:35 -0700

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,79 +1,31 @@
-    // function startbuttonClick()
+        // function startbuttonClick()
     // function stopbuttonClick()
     
-    function runtimer (x){
+    
 
-    currTime = 50;
-    var timeout = 1000;
-    
-    setTimeout(function(){
-        x.innerHTML = currTime
-        currTime = currTime - 5;
-    }, timeout);
-    timeout = timeout + 1000;
-    
-    setTimeout(function(){
-        x.innerHTML = currTime
-        currTime = currTime - 5;
-    }, timeout);
-    timeout = timeout + 1000;
-    
-    setTimeout(function(){
-        x.innerHTML = currTime
-        currTime = currTime - 5;
-    }, timeout);
-    timeout = timeout + 1000;
-    
-    setTimeout(function(){
-        x.innerHTML = currTime
-        currTime = currTime - 5;
-    }, timeout);
-    timeout = timeout + 1000;
-    
-    setTimeout(function(){
-        x.innerHTML = currTime
-        currTime = currTime - 5;
-    }, timeout);
-    timeout = timeout + 1000;
-    
-    setTimeout(function(){
-        x.innerHTML = currTime
-        currTime = currTime - 5;
-    }, timeout);
-    timeout = timeout + 1000;
-    
-    setTimeout(function(){
-        x.innerHTML = currTime
-        currTime = currTime - 5;
-    }, timeout);
-    timeout = timeout + 1000;
-    
-    setTimeout(function(){
-        x.innerHTML = currTime
-        currTime = currTime - 5;
-    }, timeout);
-    timeout = timeout + 1000;
-    
-    setTimeout(function(){
-        x.innerHTML = currTime
-        currTime = currTime - 5;
-    }, timeout);
-    timeout = timeout + 1000;
-    
-    setTimeout(function(){
-        x.innerHTML = currTime
-        currTime = currTime - 5;
-    }, timeout);
-    timeout = timeout + 1000;
-
-    setTimeout(function(){
-        x.innerHTML = currTime
-        currTime = currTime - 5;
-    }, timeout);
-    timeout = timeout + 1000;
-
-    setTimeout(function(){
-        x.innerHTML = 0, alert('Blast Off!!');
-    }, timeout);
-    timeout = timeout + 1000;
+    function runtimer (x,y){
+        var timeout = 1000;
+        var currTime = 50
+        y.innerHTML += "runtimerActive<br>"
+        for (i = 0; i < 11; i++) {
+            y.innerHTML += "forActive<br>"
+            setTimeout(function(){
+                if (currTime <= 25) {
+                    x.innerHTML = "WARNING : " + currTime + " seconds till Launch."
+                    y.innerHTML += "currTime less than/equal to 25<br>"
+                    currTime = currTime - 5
+                    y.innerHTML += "currTime Reduced<br>"
+                } else {
+                    x.innerHTML = currTime + " seconds till Launch."
+                    y.innerHTML += "currTime greater than 25<br>"
+                    currTime = currTime - 5
+                    y.innerHTML += "currTime Reduced<br>"
+                }
+                if (currTime == 0){
+                    alert('Blast Off!!');
+                    y.innerHTML += "LaunchAlert<br>"
+                }
+            }, timeout);
+            timeout = timeout + 1000;
+        }
     }
