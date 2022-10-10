3ede48ea79f72e16c98876950ac27b6417022136
IPrismaticI <112722043+IPrismaticI@users.noreply.github.com>
2022-10-10 14:31:23 -0700

    Updated values to print better in debug.

--- clock.js
+++ clock.js
@@ -5,23 +5,23 @@
 
     function runtimer (x,y){
         var timeout = 1000;
-        var currTime = 50
+        var currTime = 55 //55 won't appear, since currTime is reduced by 5 before it is printed.
         y.innerHTML += "runtimerActive<br>"
         for (i = 0; i < 11; i++) {
             y.innerHTML += "forActive<br>"
             setTimeout(function(){
-                if (currTime <= 25) {
+                if (currTime <= 30) {
+                    currTime = currTime - 5
                     x.innerHTML = "WARNING : " + currTime + " seconds till Launch."
                     y.innerHTML += "currTime less than/equal to 25<br>"
-                    currTime = currTime - 5
-                    y.innerHTML += "currTime Reduced<br>"
+                    y.innerHTML += "currTime Reduced to " + currTime + "<br>"
                 } else {
+                    currTime = currTime - 5
                     x.innerHTML = currTime + " seconds till Launch."
                     y.innerHTML += "currTime greater than 25<br>"
-                    currTime = currTime - 5
-                    y.innerHTML += "currTime Reduced<br>"
+                    y.innerHTML += "currTime Reduced to " + currTime + "<br>"
                 }
-                if (currTime == -5){
+                if (currTime == 0){
                     alert('Blast Off!!');
                     y.innerHTML += "LaunchAlert<br>"
                 }
